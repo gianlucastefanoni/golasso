982a08ee6ec527af061959bdf28c3d222e06e2b0
gianlucastefanoni <gianlucastefanoni19@gmail.com>
2026-08-04 15:13:54 +0200

    pwa plugin

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.tsx'
+
+registerSW({
+  onOfflineReady() {
+    console.log('PWA pronta offline')
+  },
+  onNeedRefresh() {
+    console.log('Nuova versione disponibile')
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Golasso",
+        short_name: "Golasso",
+        description: "Golasso",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/golasso/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/golasso/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+
   base: "/golasso/",
 });
